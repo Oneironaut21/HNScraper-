aaa5271542e108a604e525e6bf5011a86be8dcc5
Matthew Blinow <mblinow@gmail.com>
2020-06-02 11:13:11 -0400

    Package Updates

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # HNScraper-
+
+This application interfaces with the HackerNews API to display the latest stories on an Angular frontend
---
+++ Tests/StoryControllerTest.cs
@@ -0,0 +1,21 @@
+using HNScraper;
+using Moq;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test()
+        {
+            //Testing TBD
+        }
+    }
+}
