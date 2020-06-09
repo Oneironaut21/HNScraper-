17bc0564ea9d20a6559cbce37a00e1c8f6a277f1
Matthew Blinow <mblinow@gmail.com>
2020-06-09 15:53:15 -0400

    Starting implementation of storycollection class to better organize methods that handle this list and reign in responsibilities of the main controller

---
+++ HNScraper/Models/StoryCollection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HNScraper.Models
+{
+    public class StoryCollection
+    {
+        public List<StoryModel> Stories { get; private set; }
+
+        public StoryCollection()
+        {
+            Stories = new List<StoryModel>();
+        }
+        public void Add(StoryModel newStory)
+        {
+            Stories.Add(newStory);
+        }
+
+    }
+}
---
+++ Tests/StoryCollectionTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HNScraper;
+using HNScraper.Models;
+using NUnit.Framework;
+
+namespace Tests
+{
+    class StoryCollectionTests
+    {
+        [Test]
+        public void AddStoryTest()
+        {
+            var storyColl = new StoryCollection();
+            StoryModel newModel = new StoryModel();
+
+            newModel.Title = "foo";
+            newModel.Url = "www.google.com";
+            newModel.By = "John Doe";
+
+            storyColl.Add(newModel);
+
+            Assert.IsTrue(storyColl.Stories.Count == 1);
+        }
+    }
+}
